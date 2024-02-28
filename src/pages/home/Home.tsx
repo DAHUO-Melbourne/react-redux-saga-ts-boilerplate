c469bc5b0aef6761f2e66d86a0d90f90b85c63b5
DaHuo-Melbourne <huodamelbourne@gmail.com>
2024-02-28 23:36:16 +1100

    feat: add page jumping function

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 const Home = () => {
+  const history = useHistory();
   return (
     <div>
       this is home page for react-redux-ts-redux-saga-boilerplate
+      <button onClick={() => history.push('/design')}>press it</button>
     </div>
   );
 }
